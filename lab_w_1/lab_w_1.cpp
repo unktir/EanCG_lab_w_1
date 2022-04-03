adba01cca23485c25619236fb3a08012eafb9ea5
unktir <valera47304@gmail.com>
2022-04-03 16:07:36 +0500

    Step 1

--- lab_w_1/lab_w_1.cpp
+++ lab_w_1/lab_w_1.cpp
@@ -1,6 +1,32 @@
-﻿#include <iostream>
+﻿#include <GL/freeglut.h>
 
-int main()
+/*	Функция рендера сцены	*/
+static void RenderScene()
 {
-    std::cout << "Hello World!\n";
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSwapBuffers();
+}
+
+/*	Главная функция	*/
+int main(int argc, char** argv)
+{
+	/*	Инициализация первоначального состояния GLUT	*/
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInit(&argc, argv);
+
+	/*	Создаём окно	*/
+	glutCreateWindow("Lab work №1");
+
+	/*	Регистрация вызываемой функции	*/
+	glutDisplayFunc(RenderScene);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	/*	Запуск механизма обработки событий	*/
+	glutMainLoop();
+
+	return 0;
 }
